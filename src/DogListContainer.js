b12fdbaf35b3d5c2a3309f6fd6a656f121bf5b33
takatoshiinaoka <heavenhayao2316@gmail.com>
2022-08-09 22:18:28 +0900

    [React09] useEffect

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -2,6 +2,26 @@
 import React from "react";
 
 export const DogListContainer = () => {
-    const [breeds, setBreeds] = React.useState()
-    return ()
+    const [breeds, setBreeds] = React.useState([])
+    const dogListUrl = "https://dog.ceo/api/breeds/list/all"
+
+    React.useEffect(() => {
+        fetch(dogListUrl)
+        .then(res => res.json())
+        .then(data => {
+            const array = []
+            for(let i in data.message){
+                array.push(i)
+            }
+            setBreeds(array)
+        })
+    },[])
+
+    return (
+        <>
+        <select title="dogList">
+            {breeds.map((item, index) => <option value={index}>{item}</option>)}
+        </select>
+        </>
+    )
 }
